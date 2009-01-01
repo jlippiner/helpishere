60da4e07ac668918ba92c4946fb114faa47ac489
joshua.lippiner <josh@helpishere.org>
2009-01-01 11:29:48 -0500

    Updated development.rb for Waynes server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,10 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
-GOOGLE_MAPS_KEY = "ABQIAAAA-ClHV9cqqgGz8JA-fGno7BT2yXp_ZAY8_ufC3CFXhHIE1NvwkxQn86O7CT-qQfO7vzqImieuTTYERA"
-
+if (request.host=='localhost')
+  GOOGLE_MAPS_KEY = "ABQIAAAA-ClHV9cqqgGz8JA-fGno7BT2yXp_ZAY8_ufC3CFXhHIE1NvwkxQn86O7CT-qQfO7vzqImieuTTYERA"
+else
+  GOOGLE_MAPS_KEY = "ABQIAAAAMKm5Yv65vhZQlBwb7lyQUBSqsR9f-x1HZ2VWWhkUm3d7g6SwJBStBRRPOqqUymPtywSwm04Yz_elJw"
+end
 # In the development environment your application's code is reloaded on
 # every request.  This slows down response time but is perfect for development
 # since you don't have to restart the webserver when you make code changes.
