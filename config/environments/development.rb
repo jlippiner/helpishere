4ad16b9daa79b1d77fa105778886ed9dd2e4c4c4
joshua.lippiner <josh@helpishere.org>
2009-01-30 15:42:39 -0500

    Fixing server related issues with Paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,8 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-Paperclip.options[:command_path] = '/opt/local/bin'
+if (request.host=="localhost")
+  Paperclip.options[:command_path] = '/opt/local/bin'
+end
 
 require 'ruby-debug'
